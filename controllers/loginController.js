452461aabe4006e85e8b9269d53dbe99c5412501
Ritaakterlaboni <ritaakterlaboni4@gmail.com>
2026-01-21 22:22:22 +0600

    resendOtp login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,5 +1,22 @@
-function loginController(){
+const userSchema = require("../model/userSchema");
 
+
+async function loginController(req, res){
+    const {email, password} = req.body
+    if(!email){
+        return res.send("email is required");
+    }
+    if(!password){
+        return res.send("password is required");
+    }
+    const duplicateUser = await userSchema.findOne({email})
+    if(!duplicateUser){
+        return res.send("email not found")
+    }
+    //  if(!duplicateUser.password){
+    //     return res.send("password is not match")
+    // }
+    return res.end("sas")
 }
 
 module.exports = loginController
